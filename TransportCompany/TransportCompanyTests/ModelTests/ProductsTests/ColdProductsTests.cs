7bc0a3b8f242b3fea4df7340788fa44de0ec3f4d
denforplay <denforplay@mail.ru>
2021-10-26 19:35:53 +0300

    Products tests

---
+++ TransportCompany/TransportCompanyTests/ModelTests/ProductsTests/ColdProductsTests.cs
@@ -0,0 +1,15 @@
+﻿using ProductsLib.Models.Products.NeedColdProducts;
+using System;
+using Xunit;
+
+namespace TransportCompanyTests.ModelTests.ProductsTests
+{
+    public class ColdProductsTests
+    {
+        [Fact]
+        public void CreateMilkProduct_LowerTemperetureHigherThanHigherTemperature_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new Milk(1, 5, 0));
+        }
+    }
+}
---
+++ TransportCompany/TransportCompanyTests/ModelTests/ProductsTests/FuelProductsTests.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace TransportCompanyTests.ModelTests.ProductsTests
+{
+    public class FuelProductsTests
+    {
+    }
+}
