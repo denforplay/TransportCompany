98a30410345d5784d879c76997be92c10b1e051f
denforplay <denforplay@mail.ru>
2021-10-26 19:36:24 +0300

    Added autopark

---
+++ TransportCompany/TransportCompany/Models/AutoPark.cs
@@ -0,0 +1,13 @@
+﻿using ProductsLib.Models.Products;
+using System.Collections.Generic;
+using TransportCompanyLib.Models.Semitrailers;
+using TransportCompanyLib.Models.SemitrailerTractors;
+
+namespace TransportCompanyLib.Models
+{
+    public sealed class AutoPark
+    {
+        private List<SemitrailerTractorBase> _semitrailerTractors;
+        private List<SemitrailerBase<ProductBase>> _semitrailers;
+    }
+}
--- TransportCompany/TransportCompany/Models/TransportCompany.cs
+++ TransportCompany/TransportCompany/Models/TransportCompany.cs
@@ -6,7 +6,6 @@ namespace TransportCompanyLib.Models
 {
     public sealed class TransportCompany
     {
-        private List<SemitrailerTractorBase> _semitrailerTractors;
-        private List<SemitrailerBase> _semitrailers;
+        private AutoPark _autoPark;
     }
 }
