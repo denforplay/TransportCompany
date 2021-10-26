fe63ee53223ffcd0f1ec3373dc0ee6a7bfc2a8da
denforplay <denforplay@mail.ru>
2021-10-26 19:34:43 +0300

    Added base semitrailer

--- TransportCompany/TransportCompany/Models/Semitrailers/SemitrailerBase.cs
+++ TransportCompany/TransportCompany/Models/Semitrailers/SemitrailerBase.cs
@@ -1,23 +1,40 @@
-﻿using System;
-using TransportCompanyLib.Models.Products;
+﻿using ProductsLib.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TransportCompanyLib.Models.Semitrailers
 {
-    public abstract class SemitrailerBase
+    public abstract class SemitrailerBase<T> where T : ProductBase
     {
-        private ProductBase _semitrailerProduct;
-        private int _carryingCapacity;
+        protected List<T> _semitrailerProducts;
+        private float _maxCarryingWeight;
+        public float CurrentProductsWeight => _semitrailerProducts.Sum(x => x.WeightPerProduct);
+        public float MaxCarryingWeight => _maxCarryingWeight;
 
-        public int CarryingCapacity;
-
-        public SemitrailerBase(int carryingCapacity, ProductBase semitrailerProduct)
+        public SemitrailerBase(float maxCarryingWeight)
         {
-            if (carryingCapacity <= 0)
+            if (maxCarryingWeight <= 0)
             {
-                throw new ArgumentException(nameof(carryingCapacity));
+                throw new ArgumentException(nameof(maxCarryingWeight));
             }
 
-            _carryingCapacity = carryingCapacity;
+            _semitrailerProducts = new List<T>();
+            _maxCarryingWeight = maxCarryingWeight;
+        }
+
+        /// <summary>
+        /// Method to load product in semitrailer
+        /// </summary>
+        public abstract void Load(T product, int count);
+
+        public void Unload(int productCount)
+        {
+            while(productCount-- != 0 && _semitrailerProducts.Count != 0)
+            {
+                var product = _semitrailerProducts.Last();
+                _semitrailerProducts.Remove(product);
+            }
         }
     }
 }
